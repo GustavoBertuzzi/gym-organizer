bb098c094dbef4ef453c4992ce18e8746a3452fa
gustavo_bertuzzi <gustavo_bertuzzi@hst.com.br>
2025-09-30 15:29:19 -0300

    Configurando os mappers

--- src/main/java/br/com/gustavo/gym/organizer/dto/responseDTO/WorkoutExerciseResponseDTO.java
+++ src/main/java/br/com/gustavo/gym/organizer/dto/responseDTO/WorkoutExerciseResponseDTO.java
@@ -2,8 +2,9 @@ package br.com.gustavo.gym.organizer.dto.responseDTO;
 
 public record WorkoutExerciseResponseDTO(
         Long workoutExerciseId,
+        ExercisesResponseDTO exerciseDTO,
         Double weight,
         Integer repetitions,
         Integer sets,
-        Integer breakTime, ExercisesResponseDTO exerciseDTO) {
+        Integer breakTime) {
 }
